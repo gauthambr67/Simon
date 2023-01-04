dd270248265ce0403ff89fbed26291bd00db8c32
Gautham Battineni <71089271+gauthambr67@users.noreply.github.com>
2023-01-04 17:59:04 -0500

    updating progress

---
+++ script.js
@@ -0,0 +1,5 @@
+const startEl = document.getElementById("startBtn");
+const btn1El = document.getElementById("btn1");
+const btn2El = document.getElementById("btn2");
+const btn3El = document.getElementById("btn3");
+const btn4El = document.getElementById("btn4");
