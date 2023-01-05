ea594af95c16a245c41258906a03106ee50dfdc9
Gautham Battineni <71089271+gauthambr67@users.noreply.github.com>
2023-01-05 01:18:48 -0500

    updating work

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const btn1El = document.getElementById("btn1");
 const btn2El = document.getElementById("btn2");
 const btn3El = document.getElementById("btn3");
 const btn4El = document.getElementById("btn4");
+const btnSet = [btn1El, btn2El, btn3El, btn4El];
+let btnIdx = 0;
+
+function btnOrder(cb) {
+  btnSet.forEach((el) => (el.style.opacity = "100%"));
+  btnSet[btnIdx].style.opacity = "100%";
+
+  setTimeout(cb, 300);
+}
